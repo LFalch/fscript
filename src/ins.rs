88a7fc149cdbcc67fcb7a19e4f6057390cd8776e
LFalch <lucas@wasd.dk>
2020-10-04 17:26:26 +0200

    Add basic instructions

---
+++ src/ins.rs
@@ -0,0 +1,170 @@
+use std::io::{Write, Read, Error as IoError};
+use std::mem::size_of;
+
+use byteorder::{ByteOrder, LittleEndian};
+
+use super::types::{Offset, Pointer};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Instruction {
+    PushLiteral(Vec<u8>),
+    PushPop{
+        position: Offset,
+        size: Offset,
+    },
+    Pop(Offset),
+    Call(Pointer),
+    Return,
+}
+
+impl Instruction {
+    pub fn encoded_len(&self) -> usize {
+        use self::Instruction::*;
+        1 + match self {
+            PushLiteral(v) => size_of::<Offset>() + v.len(),
+            PushPop{..} => 2 * size_of::<Offset>(),
+            Pop(_) => size_of::<Offset>(),
+            Call(_) => size_of::<Pointer>(),
+            Return => 0,
+        }
+    }
+}
+
+pub mod opcode {
+    pub const RET: u8 = 0;
+    pub const CALL: u8 = 1;
+    pub const POP: u8 = 2;
+    pub const PUSH_LIT: u8 = 3;
+    pub const PUSH_POP: u8 = 4;
+}
+
+#[derive(Debug)]
+pub enum Error {
+    InvalidOpcode,
+    IoError(IoError),
+}
+
+impl From<IoError> for Error {
+    #[inline(always)]
+    fn from(io: IoError) -> Self {
+        Error::IoError(io)
+    }
+}
+
+#[derive(Debug, Copy, Clone, Default, PartialEq, Eq)]
+pub struct InstructionCoder<T>(pub T);
+
+impl<T> InstructionCoder<T> {
+    #[inline(always)]
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+}
+
+impl<T: Write> InstructionCoder<T> {
+    pub fn write_ins(&mut self, ins: Instruction) -> Result<(), Error> {
+        use self::Instruction::*;
+        match ins {
+            Return => self.0.write_all(&[opcode::RET])?,
+            Call(p) => {
+                let mut to_write = [opcode::CALL, 0, 0, 0, 0, 0, 0, 0, 0];
+                LittleEndian::write_u64(&mut to_write[1..], p.into());
+                self.0.write_all(&to_write)?;
+            }
+            Pop(o) => {
+                let mut to_write = [opcode::POP, 0, 0, 0, 0, 0, 0, 0, 0];
+                LittleEndian::write_u64(&mut to_write[1..], o as u64);
+                self.0.write_all(&to_write)?;
+            }
+            PushPop{position, size} => {
+                let mut to_write = [opcode::PUSH_POP, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+                LittleEndian::write_u64(&mut to_write[1..9], position as u64);
+                LittleEndian::write_u64(&mut to_write[9..17], size as u64);
+                self.0.write_all(&to_write)?;
+            }
+            PushLiteral(v) => {
+                let mut to_write = [opcode::PUSH_LIT, 0, 0, 0, 0, 0, 0, 0, 0];
+                LittleEndian::write_u64(&mut to_write[1..], v.len() as u64);
+                self.0.write_all(&to_write)?;
+                self.0.write_all(&v)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+impl<T: Read> InstructionCoder<T> {
+    pub fn read_ins(&mut self) -> Result<Instruction, Error> {
+        let mut opcode = [0];
+        self.0.read_exact(&mut opcode)?;
+        let [opcode] = opcode;
+        match opcode {
+            opcode::RET => Ok(Instruction::Return),
+            opcode::CALL => {
+                let mut location = [0; 8];
+                self.0.read_exact(&mut location)?;
+                let location = LittleEndian::read_u64(&location);
+                Ok(Instruction::Call(location.into()))
+            }
+            opcode::POP => {
+                let mut offset = [0; 8];
+                self.0.read_exact(&mut offset)?;
+                let offset = LittleEndian::read_u64(&offset) as usize;
+                Ok(Instruction::Pop(offset))
+            }
+            opcode::PUSH_POP => {
+                let mut buf = [0; 16];
+                self.0.read_exact(&mut buf)?;
+                let position = LittleEndian::read_u64(&buf[..8]) as usize;
+                let size = LittleEndian::read_u64(&buf[8..]) as usize;
+                Ok(Instruction::PushPop{position, size})
+            }
+            opcode::PUSH_LIT => {
+                let mut length = [0; 8];
+                self.0.read_exact(&mut length)?;
+                let length = LittleEndian::read_u64(&length) as usize;
+
+                let mut v = Vec::with_capacity(length);
+                v.resize(length, 0);
+                
+                self.0.read_exact(&mut *v)?;
+
+                Ok(Instruction::PushLiteral(v))
+            }
+            _ => Err(Error::InvalidOpcode),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+    use super::*;
+    #[test]
+    fn write_read() {
+        let inss = [
+            Instruction::Pop(16),
+            Instruction::Call(Pointer::text(0xdead)),
+            Instruction::PushLiteral(vec![b'h', b'e', b'l', b'l', b'o']),
+            Instruction::PushPop{position: 16, size: 4},
+            Instruction::Return,
+        ];
+        let mut coder = InstructionCoder(Vec::<u8>::new());
+        for ins in &inss {
+            coder.write_ins(ins.clone()).unwrap();
+        }
+        let v = coder.into_inner();
+        assert_eq!(v.len(), inss.iter().map(Instruction::encoded_len).sum());
+        let mut coder = InstructionCoder(Cursor::new(dbg!(v)));
+        let mut inss_read = Vec::with_capacity(inss.len());
+        loop {
+            let ins = coder.read_ins().unwrap();
+            let done = matches!(&ins, Instruction::Return);
+            inss_read.push(ins);
+            if done {
+                break
+            }
+        }
+        assert_eq!(&inss[..], &*inss_read);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+use std::num::NonZeroUsize;
+use std::io::{Read, Error as IoError};
+
+#[cfg(not(target_pointer_width = "64"))]
+compile_error!("Does not support anything other than 64-bit");
+
 pub mod types {
     use std::num::NonZeroUsize;
     pub type Unit = ();
@@ -7,7 +13,9 @@ pub mod types {
     pub type Float = f64;
     #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
     pub struct Pointer(pub(super) NonZeroUsize);
+    pub type Offset = usize;
     pub const         MASK: usize = 0b1100_0000 << (64 - 8);
+    pub const SECTION_SIZE: usize = 0b0100_0000 << (64 - 8);
     pub const    TEXT_MASK: usize = 0b0000_0000 << (64 - 8);
     pub const  HEAP_OFFSET: usize = 0b0100_0000 << (64 - 8);
     pub const STACK_OFFSET: usize = 0b1000_0000 << (64 - 8);
@@ -15,6 +23,7 @@ pub mod types {
     impl Pointer {
         #[inline(always)]
         pub const fn text(i: usize) -> Self {
+            debug_assert_ne!(i, 0);
             Pointer(unsafe{NonZeroUsize::new_unchecked(i | TEXT_MASK)})
         }
         #[inline(always)]
@@ -26,10 +35,24 @@ pub mod types {
             Pointer(unsafe{NonZeroUsize::new_unchecked(i | STACK_OFFSET)})
         }
     }
+    impl From<Pointer> for u64 {
+        #[inline]
+        fn from(p: Pointer) -> Self {
+            p.0.get() as u64
+        }
+    }
+    impl From<u64> for Pointer {
+        #[inline]
+        fn from(p: u64) -> Self {
+            debug_assert_ne!(p, 0);
+            Pointer(unsafe{NonZeroUsize::new_unchecked(p as usize)})
+        }
+    }
 }
 mod type_type;
 pub use type_type::*;
 use types::Pointer;
+pub mod ins;
 
 use std::{
     alloc::{alloc, dealloc, realloc, Layout},
@@ -77,8 +100,8 @@ impl MemoryArea {
         assert!(self.size < new_size);
         self.set_size(new_size);
     }
-    pub fn deref(&mut self, index: usize, typ: &Type) -> Option<*mut u8> {
-        if index + typ.size() > self.size {
+    pub fn deref(&mut self, index: usize, size: usize) -> Option<*mut u8> {
+        if index + size > self.size {
             None
         } else {
             unsafe { Some(self.buf.add(index)) }
@@ -109,20 +132,42 @@ impl Clone for MemoryArea {
 struct RuntimeEnvironment {
     symbol_table: HashMap<(String, Type), Pointer>,
     stack_pointer: Pointer,
+    instruction_pointer: Pointer,
     text: MemoryArea,
     heap: MemoryArea,
     stack: MemoryArea,
 }
 impl RuntimeEnvironment {
-    pub fn deref(&mut self, p: Pointer, typ: &Type) -> Option<*mut u8> {
+    pub fn next_ins(&mut self) -> ins::Instruction {
+        ins::InstructionCoder(self).read_ins().unwrap()
+    }
+    pub fn deref(&mut self, p: Pointer, size: usize) -> Option<*mut u8> {
         let i = p.0.get();
         let masked = i & types::MASK;
 
         match masked.count_ones() {
-            0 => self.text.deref(i, typ),
-            1 => self.heap.deref(i - types::HEAP_OFFSET, typ),
-            2 => self.stack.deref(i & (!types::MASK), typ),
+            0 => self.text.deref(i, size),
+            1 => self.heap.deref(i - types::HEAP_OFFSET, size),
+            2 => self.stack.deref(i & (!types::MASK), size),
             _ => unreachable!(),
+        }
+    }
+}
+
+impl Read for RuntimeEnvironment {
+    #[inline(always)]
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, IoError> {
+        self.read_exact(buf).map(|_| buf.len())
+    }
+    fn read_exact(&mut self, buf: &mut [u8]) -> Result<(), IoError> {
+        let len = buf.len();
+        match self.deref(self.instruction_pointer, len) {
+            Some(p) => unsafe {
+                self.instruction_pointer.0 = NonZeroUsize::new_unchecked(self.instruction_pointer.0.get() + len);
+                ptr::copy_nonoverlapping(p, buf.as_mut_ptr(), len);
+                Ok(())
+            }
+            None => Err(IoError::from(std::io::ErrorKind::NotFound))
         }
     }
 }
